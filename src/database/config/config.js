4fb025758190269ebc84265e8ea50052671907c1
Facundo Damian De Benedetti <facundodebenedetti09@hotmail.com>
2022-02-24 11:26:04 -0300

    modificado config probando si funca asi

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "vb591lxl8i7320a2",
-    "password": "td44u0m8axmbd5r0",
+    "password": td44u0m8axmbd5r0,
     "database": "z9bceftpnjmcli4q",
     "host": "uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
