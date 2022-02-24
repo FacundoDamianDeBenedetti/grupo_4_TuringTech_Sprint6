de96c0ba91f99b9a50910b1f934989ed3dded23a
Facundo Damian De Benedetti <facundodebenedetti09@hotmail.com>
2022-02-24 11:24:26 -0300

    modificado config probando si funca asi

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "turingtechdb",
-    "host": "127.0.0.1",
+    "username": "vb591lxl8i7320a2",
+    "password": "td44u0m8axmbd5r0",
+    "database": "z9bceftpnjmcli4q",
+    "host": "uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "test": {
