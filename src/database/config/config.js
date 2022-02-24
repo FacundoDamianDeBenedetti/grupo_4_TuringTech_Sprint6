d0d632ea2c7f6833792c09b90fc297ef097fa9bc
Facundo Damian De Benedetti <facundodebenedetti09@hotmail.com>
2022-02-24 11:24:45 -0300

    modificado config probando si funca asi

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,19 +5,5 @@ module.exports = {
     "database": "z9bceftpnjmcli4q",
     "host": "uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
   }
 }
