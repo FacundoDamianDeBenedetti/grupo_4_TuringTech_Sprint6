d4a35c7b84e83a94cc0d858fb1507e4008ba9d03
Facundo Damian De Benedetti <facundodebenedetti09@hotmail.com>
2022-02-24 11:47:25 -0300

    modificado config probando si funca asi x3

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,18 +1,20 @@
-module.exports = {
-  "development": {
-    "username": "vb591lxl8i7320a2",
-    "password": "td44u0m8axmbd5r0",
-    "database": "z9bceftpnjmcli4q",
-    "host": "uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+require('dotenv').config()
+module.exports =
+{
+    "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "vb591lxl8i7320a2",
-    "password": "td44u0m8axmbd5r0",
-    "database": "z9bceftpnjmcli4q",
-    "host": "uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "dialect": process.env.DB_DIALECT,
+},
+    "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
-    "dialect": "mysql"
+    "dialect": process.env.DB_DIALECT
 }
 }
