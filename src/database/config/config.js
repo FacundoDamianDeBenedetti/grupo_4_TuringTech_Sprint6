0bfb1f34b5f1ec9043dec1756097cba11dd3541a
Facundo Damian De Benedetti <facundodebenedetti09@hotmail.com>
2022-02-26 12:16:16 -0300

    probando cambios config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,19 +2,19 @@ require('dotenv').config()
 module.exports =
 {
     "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT,
+    "username": "vb591lxl8i7320a2",
+    "password": "td44u0m8axmbd5r0",
+    "database": "z9bceftpnjmcli4q",
+    "host": "uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "port": "3306",
+    "dialect": "mysql",
 },
     "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT
+        "username": "vb591lxl8i7320a2",
+        "password": "td44u0m8axmbd5r0",
+        "database": "z9bceftpnjmcli4q",
+        "host": "uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        "port": "3306",
+        "dialect": "mysql",
 }
 }
